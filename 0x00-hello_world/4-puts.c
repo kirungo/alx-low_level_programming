a121041a93a41402e9032695be8a2b69af148ba9
kirungo <terrikirungo@gmail.com>
2025-10-12 10:47:45 -0700

    Add the \ to escape the double-quote at the start

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(){
-puts("Programming is like building a multilingual puzzle,");
-return 0;
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle,"
+ * using puts
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
